885cdd2f0d41b8bc57fac1b27ec553700c572d0b
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-04-25 18:05:36 -0300

    feat: create archive and questao1 solved

---
+++ java/wikiPython/listas/Questao1.java
@@ -0,0 +1,14 @@
+/*
+ * Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+ */
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Questao1 {
+    public static void main(String[] args) {
+        int[] vector = {1,2,3,4,5};
+        System.out.print(Arrays.toString(vector)); 
+    }
+ }
