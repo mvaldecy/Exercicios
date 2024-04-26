48943f57f297b0e77eaa3d168cf10c7a709de22e
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-04-26 16:05:21 -0300

    feat: Questao 2 resolvida

---
+++ java/Exercicios-Java/lista_1/2/Questao2.java
@@ -0,0 +1,24 @@
+/*
+ * Faça um programa que: 
+- Leia a cotação do dólar 
+- Leia um valor em dólares 
+- Converta esse valor para Real 
+- Mostre o resultado 
+ */
+
+import java.util.Scanner;
+
+public class Questao2 {
+    public static void main(String[] args) {
+        float cotacao_dolar, input_dolar, valor_real;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inira a cotação do dolar em real:");
+        cotacao_dolar = scanner.nextFloat();
+        System.out.println("Insira o valor em dolar a ser convertido em real");
+        input_dolar = scanner.nextFloat();
+        valor_real = cotacao_dolar * input_dolar;
+        System.out.printf("O valor em real é R$ %.2f\n", valor_real);
+        
+    }
+    
+}
